53948eccb0c17f42e04438188e47fec9a248ef19
crystal025 <oj3339@naver.com>
2022-10-07 12:22:01 +0900

    #212 hotfix.메인에 새로고침 해야 게시물 뜨도록 되돌리기

--- src/components/mainComponents/AllPosts.jsx
+++ src/components/mainComponents/AllPosts.jsx
@@ -18,24 +18,16 @@ const AllPosts = () => {
   const allMainPosts = [...new Set(allMainPostList.map(JSON.stringify))].map(
     JSON.parse
   );
-  const isPosting = useSelector((state) => state.upload.addPosting);
 
-  // useCallback을 사용하는 이유는 해당 스크롤 이벤트 자체가
-  // 상위 컴포넌트의 '값 변동(스크롤을 내림으로 값이 추가됨)'으로 인해
-  // 여러번 불러와지게 되는데 그 것을 최초 1번만 불러와서
-  // 변동사항 없으면 계속 쓰기 위함이다.
   const getMainList = useCallback(() => {
     async function getMainData() {
       await dispatch(__getMainAllPosts({ paging: paging, userId: userId })); //api요청
       setLoading(false); //요청하고나면 loading false로
     }
     return getMainData();
-  }, [paging, allMainPosts, isPosting]);
+  }, [paging, allMainPosts]);
 
-  // 스크롤위치 계산시 연산 너무 많이되는 것
-  // 방지하기 위해 500ms 쓰로틀적용 (0.5초 마다 보낸다.)
-  // 스크롤 하단부가 100px 이하에 위치한다는 조건이 만족할 시
-  // 일정 시간마다 요청을 보낸다.
+  // 스크롤위치 계산
   const _handleScroll = _.throttle(() => {
     const scrollHeight = document.documentElement.scrollHeight;
     const scrollTop = document.documentElement.scrollTop;
@@ -53,14 +45,14 @@ const AllPosts = () => {
   }, 500);
 
   useEffect(() => {
-    if ((paging === 1 && allMainPosts.length === 0) || isPosting === true) {
+    if (paging === 1 && allMainPosts.length === 0) {
       dispatch(__getMainAllPosts({ paging: paging, userId: userId }));
       setPaging(paging + 1);
     } //첫렌더링시 0페이지 받아오기
     if (allMainPosts.length !== 0) {
-      setPaging(allMainPosts.length / 8 + 1);
+      setPaging(allMainPosts.length);
     } //다른컴포넌트 갔다 올때 렌더링시 페이지넘버 계산
-  }, [isPosting]);
+  }, []);
 
   useEffect(() => {
     if (loading) {
@@ -75,11 +67,9 @@ const AllPosts = () => {
   return (
     <Fragment>
       <List>
-        {allMainPosts
-          ?.sort((a, b) => b.postId - a.postId)
-          .map((item, idx) => (
-            <EachPost key={idx} item={item} />
-          ))}
+        {allMainPosts?.map((item, idx) => (
+          <EachPost key={idx} item={item} />
+        ))}
       </List>
       {loading ? <InfinityScrollLoader /> : ""}
     </Fragment>
